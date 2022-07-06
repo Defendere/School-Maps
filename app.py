be2efbf9f553f668791dd84c7b62a6ce08cf7a24
ollielynas <lynasollie@gmail.com>
2022-07-06 13:15:13 +1200

    Added classes

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import cherrypy
 
 
+class Point:
+    def init(self, x, y):
+        self.x = x
+        self.y = y
+
+class Square:
+    def init(self, ):
+        ...
+
+class block:
+    def init(self, p1, p2, p3, p4, color):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+        self.color = color
+
+class Classroom:
+    def __init__(self, p1, p2, p3, p4, block):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.p4 = p4
+        self.block = block
+
+
 class HelloWorld(object):
     
     @cherrypy.expose
